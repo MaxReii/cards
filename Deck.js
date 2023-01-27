11bbf8448cd03d77b9234349d923bb606d36afdf
Maxime Cuche <maxime.cuche@heig-vd.ch>
2023-01-27 13:56:09 +0100

    first commit

---
+++ Deck.js
@@ -0,0 +1,33 @@
+import Card from "./Card.js";
+import shuffle from "./node_modules/lodash-es/shuffle.js";
+
+export class Deck {
+  #cards = [];
+  constructor(options) {
+    thisvalues = options.values;
+    this.suits = options.suits;
+  }
+
+  createFullDeck() {
+    this.values.forEach((v) => {
+      this.suits.forEach((s) => {
+        this.#cards.push(new Card(v, s));
+      });
+    });
+    return this;
+  }
+
+  shuffle() {
+    this.#cards = shuffle(this.#cards);
+    return this;
+  }
+
+  displayCards() {
+    this.#cards.forEach((c) => {
+      c.display();
+    });
+    return this;
+  }
+}
+
+export default Deck;
